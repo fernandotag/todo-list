5a6d865b7a9d78c683ba667ed1942b9ea63efbaa
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2022-12-17 16:40:19 -0300

    feat(task): add plus icon on button

--- src/components/FormBar.tsx
+++ src/components/FormBar.tsx
@@ -1,10 +1,14 @@
 import styles from './FormBar.module.css';
+import { Plus } from 'phosphor-react';
 
 export function FormBar() {
     return (
         <form className={styles.form}>
             <input placeholder='Adicione uma nova tarefa'></input>
-            <button type="submit">Criar</button>
+            <button type="submit">
+                <span>Criar</span>
+                <Plus size={15} weight="bold" className={styles.plusIcon} />
+            </button>
         </form>
     )
 }
