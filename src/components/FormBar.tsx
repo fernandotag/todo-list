578780b6719883191e1437c6f1ffae08c48e79ef
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2022-12-19 12:28:32 -0300

    feat(task): add new task

--- src/components/FormBar.tsx
+++ src/components/FormBar.tsx
@@ -1,11 +1,46 @@
 import styles from './FormBar.module.css';
 import { Plus } from 'phosphor-react';
+import { ITask } from '../App';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 
-export function FormBar() {
+interface IFormBarProps {
+    onCreate: (newTask: ITask) => void;
+}
+
+export function FormBar({onCreate}: IFormBarProps) {
+    const [newTaskTitle, setNewTaskTitle] = useState('');
+
+    function handleCrateNewTask(event: FormEvent) {
+        event.preventDefault();
+        const newTask = {
+            id: uuidv4(),
+            title: newTaskTitle,
+            isDone: false,
+        };
+
+        onCreate(newTask);
+        setNewTaskTitle('');
+    }
+
+    function handleTypeNewTaskTitleText(event: ChangeEvent<HTMLInputElement>) {
+        event.target.setCustomValidity("");
+        setNewTaskTitle(event.target.value);
+    }
+
+    function handleNewTaskTitleInvalid(event: any) {
+        event.target.setCustomValidity("Esse campo é obrigatório");
+    }
+
     return (
-        <form className={styles.form}>
-            <input placeholder='Adicione uma nova tarefa'></input>
-            <button type="submit">
+        <form onSubmit={handleCrateNewTask} className={styles.form}>
+            <input 
+                onChange={(e) => handleTypeNewTaskTitleText(e) }
+                required
+                onInvalid={handleNewTaskTitleInvalid}
+                placeholder='Adicione uma nova tarefa'
+            />
+            <button type="submit" >
                 <span>Criar</span>
                 <Plus size={15} weight="bold" className={styles.plusIcon} />
             </button>
