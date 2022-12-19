bb14fe34017064469593a7898b263c9fa69e1f4a
Fernando Tagliaferro <4048111+fernandotag@users.noreply.github.com>
2022-12-19 17:53:44 -0300

    feat(task): create empty list message

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -1,6 +1,8 @@
+import { ClipboardText } from 'phosphor-react';
 import { ITask } from '../App';
 import { TaskRow } from './TaskRow';
 import styles from './TaskTable.module.css';
+import clipboardIcon from '../assets/clipboard.svg';
 
 interface ITaskTableProps {
     tasks: ITask[];
@@ -8,7 +10,7 @@ interface ITaskTableProps {
     onDelete: (taskId: string) => void;
 }
 
-export function TaskTable({tasks, onSelect, onDelete}: ITaskTableProps) {
+export function TaskTable({ tasks, onSelect, onDelete }: ITaskTableProps) {
     const totalCreatedTasks = tasks.length;
     const totalDoneTasks = tasks.filter(task => task.isDone).length;
 
@@ -41,16 +43,26 @@ export function TaskTable({tasks, onSelect, onDelete}: ITaskTableProps) {
                 </tr>
             </thead>
             <tbody>
-                {tasks.map(({ id, title, isDone }: ITask) => (
-                    <TaskRow 
-                        key={id}
-                        taskId={id} 
-                        title={title}
-                        isDone={isDone}
-                        onSelect={onSelectTaskProps}
-                        onDelete={onDeleteTaskProps}
-                    />
-                ))}
+                {tasks.length === 0 ? (
+                    <div className={styles.emptyList}>
+                        <img src={clipboardIcon} />
+                        <p>
+                            <strong>You don't have any tasks registered yet.</strong><br />
+                            Create tasks and organize your to-do items
+                        </p>
+                    </div>
+                ) : (
+                    tasks.map(({ id, title, isDone }: ITask) => (
+                        <TaskRow
+                            key={id}
+                            taskId={id}
+                            title={title}
+                            isDone={isDone}
+                            onSelect={onSelectTaskProps}
+                            onDelete={onDeleteTaskProps}
+                        />
+                    ))
+                )}
             </tbody>
         </table>
     )
